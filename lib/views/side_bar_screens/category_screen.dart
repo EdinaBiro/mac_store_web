ff43d8a88778bfd028c3fb867b48864f45b87ea5
Edina Biro <edina.biro@reea.net>
2025-10-03 14:11:56 +0300

    Refactor CategoryScreen layout to display category details and input fields

--- lib/views/side_bar_screens/category_screen.dart
+++ lib/views/side_bar_screens/category_screen.dart
@@ -6,6 +6,58 @@ class CategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Vendors Screen'),);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(padding: 
+        const EdgeInsets.all(8.0),
+        child: Container(
+          alignment: Alignment.topLeft,
+        child: Text('Categories', style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),),
+        ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Divider(color: Colors.grey),
+        ),
+        Row(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Center(child: Text('Category image'),)
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 200,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText:'Enter Category Name'
+                  ),
+                ),
+              ),
+            ),
+            TextButton(onPressed: () {}, child: Text('cancel'),
+            ),
+            ElevatedButton(onPressed: (){}, child: Text('Save',
+            ),),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(onPressed: (){}, child: Text('Pick image'),),
+        ),
+        Divider(color: Colors.grey,
+        ),
+      ],
+    );
   }
 }
