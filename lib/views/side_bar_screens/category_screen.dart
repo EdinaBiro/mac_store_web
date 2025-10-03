10e83f2a6f36972b21dc426f448888be1350474b
Edina Biro <edina.biro@reea.net>
2025-10-03 16:10:56 +0300

    Refactor CategoryScreen to include form validation and category name input

--- lib/views/side_bar_screens/category_screen.dart
+++ lib/views/side_bar_screens/category_screen.dart
@@ -12,6 +12,8 @@ class CategoryScreen extends StatefulWidget {
 }
   
   class _CategoryScreenState extends State<CategoryScreen>{
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    late String categoryName;
     dynamic _image;
     pickImage() async {
       FilePickerResult? result =  await FilePicker.platform.pickFiles(
@@ -27,61 +29,76 @@ class CategoryScreen extends StatefulWidget {
     }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(padding: 
-        const EdgeInsets.all(8.0),
-        child: Container(
-          alignment: Alignment.topLeft,
-        child: Text('Categories', style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),),
-        ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Divider(color: Colors.grey),
-        ),
-        Row(
-          children: [
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(5),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(padding: 
+          const EdgeInsets.all(8.0),
+          child: Container(
+            alignment: Alignment.topLeft,
+          child: Text('Categories', style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+          ),),
+          ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Divider(color: Colors.grey),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(child: _image!=null? Image.memory(_image):
+                Text('Category image'),)
               ),
-              child: Center(child: _image!=null? Image.memory(_image):
-              Text('Category image'),)
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: 200,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText:'Enter Category Name'
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    onChanged: (value){
+                      categoryName = value;
+                    },
+                    validator: (value){
+                      if(value!.isNotEmpty){
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText:'Enter Category Name'
+                    ),
                   ),
                 ),
               ),
-            ),
-            TextButton(onPressed: () {}, child: Text('cancel'),
-            ),
-            ElevatedButton(onPressed: (){}, child: Text('Save',
-            ),),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(onPressed: (){
-            pickImage();
-          }, child: Text('Pick image'),),
-        ),
-        Divider(color: Colors.grey,
-        ),
-      ],
+              TextButton(onPressed: () {}, child: Text('cancel'),
+              ),
+              ElevatedButton(onPressed: (){
+                if(_formKey.currentState!.validate()){
+                    print(categoryName);
+                }
+              }, child: Text('Save',
+              ),),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(onPressed: (){
+              pickImage();
+            }, child: Text('Pick image'),),
+          ),
+          Divider(color: Colors.grey,
+          ),
+        ],
+      ),
     );
   }
   }
