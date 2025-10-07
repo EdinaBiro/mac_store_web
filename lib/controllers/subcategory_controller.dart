49a01494e0a67e02b5307cf8b7918c4291fb81f7
Edina Biro <edina.biro@reea.net>
2025-10-07 15:26:40 +0300

    display subcategories

--- lib/controllers/subcategory_controller.dart
+++ lib/controllers/subcategory_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:mac_store_web/global_variables.dart';
+import 'package:mac_store_web/models/category.dart';
 import 'package:mac_store_web/models/subcategory.dart';
 import 'package:http/http.dart' as http;
 import 'package:mac_store_web/services/manage_http_response.dart';
@@ -15,7 +18,7 @@ class SubcategoryController {
       Subcategory subcategory = Subcategory(id: '', categoryId: categoryId, categoryName: categoryName, image: image, subCategoryName: subCategoryName);
 
        http.Response response= await http.post(
-        Uri.parse("$uri/api/categories"),
+        Uri.parse("$uri/api/subcategories"),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -27,6 +30,27 @@ class SubcategoryController {
       });
     }catch(e){
       print("$e");
+    }
+  }
+
+    Future<List<Subcategory>> loadSubcategories() async{
+    try{
+        http.Response response = await http.get(Uri.parse('$uri/api/subcategories'),
+         headers: {
+          'Content-Type': 'application/json',
+        },
+        );
+        print(response.body);
+        if(response.statusCode==200){
+          final List<dynamic> data = jsonDecode(response.body);
+          List<Subcategory> subcategories = data.map((subcategory) => Subcategory.fromJson(subcategory)).toList();
+        return subcategories;
+        }else{
+          throw Exception('Failed to load subcategories');
+        }
+        
+    }catch(e){
+      throw Exception("Error loading categories: $e");
     }
   }
 }
